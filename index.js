03572cea559bb9f3d191fa788778971f5f4d48de
Muhammad Sabbir Hossain Bin Ahmad <dustobalok@gmail.com>
2024-11-13 20:04:47 +0600

    cors: package is installed... successfully.

--- index.js
+++ index.js
@@ -2,7 +2,15 @@
 const express = require(`express`);
 const app = express();
 require(`dotenv`).config();
+const cors = require(`cors`);
 const port = process.env.PORT || 5000;
+
+
+
+/* Express Middleware */
+app.use(cors());
+app.use(express.json());
+
 
 
 /* all routes */
