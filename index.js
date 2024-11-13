a9ba7ec303032312d11fd893dacf014d2c18eeed
Muhammad Sabbir Hossain Bin Ahmad <dustobalok@gmail.com>
2024-11-13 20:09:06 +0600

    mongodb: package is installed and connected... successfully.

--- index.js
+++ index.js
@@ -6,10 +6,47 @@ const cors = require(`cors`);
 const port = process.env.PORT || 5000;
 
 
-
 /* Express Middleware */
 app.use(cors());
 app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@provabok.zzy7f.mongodb.net/?retryWrites=true&w=majority&appName=Provabok`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+        /* All client listed routes */
+
+
+        /* All administration listed routes */
+
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 
